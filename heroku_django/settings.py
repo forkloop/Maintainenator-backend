801026a89c3fe3705973fb8590385631731941b0
forkloop <forkloop@gmail.com>
2012-09-20 20:55:17 -0400

    Fixed TEMPLATE_DIRS absolute path issue.

--- heroku_django/settings.py
+++ heroku_django/settings.py
@@ -1,5 +1,9 @@
 # Django settings for heroku_django project.
 import dj_database_url
+from os import path
+
+root = path.dirname(__file__).replace('\\', '/')
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -48,7 +52,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = root + '/../static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -98,7 +102,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/Users/forkloop/work/cse622/backend/heroku-django"
+    root + '/../static', 
 )
 
 INSTALLED_APPS = (
--- heroku_django/urls.py
+++ heroku_django/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'static/index.html'}),
-    # url(r'^heroku_django/', include('heroku_django.foo.urls')),
+    url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'index.html'}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
